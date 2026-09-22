0374a69cbaa43beea4c8a458613442a22df2ea9b
Evan Pertegaz <evanpertegaz15-pixel@users.noreply.github.com>
2026-09-22 10:43:24 +0200

    feat gestion des doublons de la bdd dans le seed

--- api/seed.py
+++ api/seed.py
@@ -1,17 +1,27 @@
 import asyncio
 from core.security import hash_password
 from db.init_db import engine, async_session
+from models.item_model import Item
 from models.user_model import User
-from sqlmodel import SQLModel
+from sqlmodel import select, SQLModel
+
+ITEMS = []
 
 async def create_db():
     print("Initialisation des tables...")
     async with engine.begin() as conn: #Connect to db and close auto
         await conn.run_sync(SQLModel.metadata.create_all) #metadata -> all tables
     print("Peuplement de la base...")
-    admin = User(email="admin@example.com", hash_password=hash_password("admin"))
     async with async_session() as session: #Open db session
-        session.add(admin)
+        for data in ITEMS:
+            result = await session.exec(select(Item).where(Item.name == data["name"]))
+            existing = result.first()
+            if existing:
+                print(f"Ignoré : {data['name']} ; item déjà présent.")
+                continue
+            item = Item(**data)
+            session.add(item)
+            print(f"Ajouté : {data['name']}")
         await session.commit()
     print("Base initialisée.")
 
