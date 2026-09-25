a5c1711cc5eee10fbd33f3f69be9489f20da0e7b
Evan Pertegaz <evanpertegaz15-pixel@users.noreply.github.com>
2026-09-25 09:15:23 +0200

    feat cors config vite

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from db.init_db import init_db
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.auth_router import auth_router
 from routers.items import items_router
 
@@ -12,3 +13,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="Collection XIXème siècle", version="0.1", lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(items_router)
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
